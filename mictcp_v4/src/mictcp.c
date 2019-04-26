b321edc3bee3fc107e2a5ca0047cafd80c597075
RubenG <rubengres@hotmail.fr>
2019-04-26 14:02:42 +0200

    Negociation taux de perte

--- mictcp_v4/src/mictcp.c
+++ mictcp_v4/src/mictcp.c
@@ -4,20 +4,22 @@
 #include <string.h>
  
 #define MAX_RESEND 50
-#define TIMEOUT 200 //en ms
+#define TIMEOUT 2000 //en ms
 #define TIMEOUTACCEPT 1000000000 //en ms, doit être suffisamment long pour lancer la source
 #define TAILLE_FENETRE_GLISSANTE 10
 #define BUFFER_SIZE 1024
 
-int derniers_messages[TAILLE_FENETRE_GLISSANTE]; //1 = message acquitté, 0 = message perdu
-float taux_perte_acceptable = 0.50;
  
 /* Définition des variables gloables */
+int derniers_messages[TAILLE_FENETRE_GLISSANTE]; //1 = message acquitté, 0 = message perdu
 mic_tcp_sock sock;
 int nextld = 0; // id du socket
 int sn = 0; // numero de sequence du message à envoyer
 int last_sn = 0; // n de sequence du dernier message recu
- 
+int taux_perte_acceptable; //en pourcentage
+int taux_seuil = 30; // taux de perte seuil pour la negociation
+int taux_demande = 25; // taux demande pour la negocation
+
 /* Permet de remplir les champs d'un pdu */
 void fill_pdu(mic_tcp_pdu * pdu,unsigned short source_port, unsigned short dest_port, unsigned int seq_num, unsigned int ack_num, unsigned char syn, unsigned char ack, unsigned char fin, char * data, int size){
     pdu->header.source_port = source_port;
@@ -39,6 +41,9 @@ int mic_tcp_socket(start_mode sm)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
    
+    /* Pourcentage de perte sur le reseau */
+    set_loss_rate(20);
+
     sock.fd = nextld++;
     sock.state = IDLE;
  
@@ -76,9 +81,8 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
     while(sock.state == WAIT_FOR_SYN);
  
     /* on choisit le taux de perte */
-    int taux_demande = 20;
-    char * payload = taux_demande;
-    int taille_payload = strlen(payload) * sizeof(char);
+    char * payload = (char *) &taux_demande;
+    int taille_payload = sizeof(char);
 
     /* on envoie le SYNACK */
     mic_tcp_pdu synack;
@@ -102,37 +106,33 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
     if(sock.state == IDLE){
-
-        int taux_seuil = 30;
- 
-        printf("addr : %s %d %d\n",addr.ip_addr, addr.ip_addr_size, addr.port);
- 
-        //construction du pdu syn
+        /* construction du pdu SYN */
         mic_tcp_pdu syn;
         fill_pdu(&syn, sock.addr.port, addr.port,0,0,1,0,0,"", 0); //initialisation du syn
  
-        //tant que l'on a pas recu de synack
+        /* Declaration du PDU que l'on va recevoie */
         mic_tcp_pdu synack;
         synack.payload.size = BUFFER_SIZE;
         synack.payload.data = malloc(synack.payload.size);
        
+        /* envoie du SYN tant que l'on a pas recu de synack */
         do{
-            if(IP_send(syn, addr) == -1) // envoie de syn
+            if(IP_send(syn, addr) == -1)
                 return -1; // erreur ipsend
         }while(IP_recv(&synack, &sock.addr, TIMEOUT) == -1);
-
-        printf("J'ai envie de mourir\n");
  
         if(synack.header.syn == 1 && synack.header.ack == 1){
-            int taux_demande = atoi(synack.payload.data);
+            /* On lit le taux de perte demande par le serveur */
+            int taux_demande = *synack.payload.data;
 
-            /* Si me taux demandé est inférieur au seuil, on le choisit sinon on garde notre seuil */
-            if(taux_demande > taux_seuil) taux_demande = taux_seuil;
+            /*Negociation du taux de perte, on applique le taux de perte le plus faible */
+            if(taux_demande < taux_seuil)
+                taux_perte_acceptable = taux_demande;
+            else
+                taux_perte_acceptable = taux_seuil;
 
-            /* On applique le taux de perte */
-            set_loss_rate(taux_demande);
-
-            printf("SYNACK recu, taux de perte accorde : %s\n", taux_demande);
+            printf("SYNACK recu\n");
+            printf("Taux de perte demande : %d, taux seuil : %d, accorde : %d\n", taux_demande, taux_seuil, taux_perte_acceptable);
  
             /* envoie de ACK */
             mic_tcp_pdu ack;
@@ -164,11 +164,11 @@ void init_fen(int * init){
     *init = 1;
 }
  
-float taux_perte(){
+int taux_perte(){
     int nb_mess_acquitte = 0;
     for(int i = 0; i < TAILLE_FENETRE_GLISSANTE; i++)
         nb_mess_acquitte += derniers_messages[i];
-    return 1 - (nb_mess_acquitte * 1.0 / TAILLE_FENETRE_GLISSANTE);
+    return (1 - (nb_mess_acquitte * 1.0 / TAILLE_FENETRE_GLISSANTE))*100;
 }
  
 /*
@@ -177,17 +177,20 @@ float taux_perte(){
  */
 int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 {
+    /* Si on n'est pas en état connecté, on n'envoie rien */
     if(sock.state != CONNECTED)
-        return -1; // pas en etat connecté, on n'envoie pas
+        return -1;
  
+    /* On initialise la fenêtre si c'est sa première utilisation */
     static int init= 0;
-    if(init == 0)   init_fen(&init);
-   
+    if(init == 0) init_fen(&init);
+
+    /* utilisé pour se situer dans la fenetre glissante */
     static int n_message = 0;
- 
+
     if(sock.fd == mic_sock){
- 
-        //construction du pdu
+        
+        /* construction du PDU */
         mic_tcp_pdu pdu;
         fill_pdu(&pdu,0,sock.addr.port,0,0,0,0,0,mesg,mesg_size);
  
@@ -198,26 +201,23 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
         mic_tcp_pdu ack;
  
         while(nb_resend < MAX_RESEND){
-            if(IP_recv(&ack, & sock.addr, TIMEOUT) == -1){ // si le timer expire
-                if(taux_perte() < taux_perte_acceptable){ //si on peut perdre le message, on ne renvoie rien
-                    derniers_messages[n_message] = 0;
-                    printf("ACK DROPPED, taux de pertes actuel : %f\n", taux_perte());
-                    break;
+            if(IP_recv(&ack, &sock.addr, TIMEOUT) == -1){   // si on ne recoit pas ack a temps
+                printf("Timer expiré\n");
+
+                if(taux_perte() < taux_perte_acceptable){   // si on peut perdre le message, on ne renvoie rien
+                    derniers_messages[n_message] = 0;       // maj de la fenetre glissante
+                    printf("Perte admissible\n");
+                } else {
+                    //renvoi du message, mécanisme du stop and wait à reprise des pertes totales
+                    nb_resend++;
+                    printf("Timeout ack, resending (%d left)... \n", MAX_RESEND - nb_resend);
+                    size = IP_send(pdu, sock.addr);
                 }
- 
-                //renvoi du message, mécanisme du stop and wait à reprise des pertes totales
-                nb_resend++;
-                printf("Timeout ack, resending (%d left)... \n", MAX_RESEND - nb_resend);
-                size = IP_send(pdu, sock.addr);
+
             } else { // on recoit le ack on quitte la boucle while
                 printf("Reception du ack n %d \n", ack.header.ack_num);
-                derniers_messages[n_message] = 1;
+                derniers_messages[n_message] = 1; // maj de la fenetre glissante
                 break;
- 
-                /*
-                verifie le numero d'acquittement (inutile pour le moment)
-                rcv_ack = ack.header.ack_num == pdu.header.seq_num
-                */
             }
         }
  
